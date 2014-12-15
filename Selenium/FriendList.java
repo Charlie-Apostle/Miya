de71199fdfb6a987d90dc43fd125ea6ae3bc3b45
Charlie-Apostle <charlie52125105!>
2014-12-15 16:44:17 +0900

    modified
    modifyd Friend List to  private

--- Selenium/FriendList.java
+++ Selenium/FriendList.java
@@ -4,23 +4,27 @@ import java.util.List;
 import java.util.regex.Pattern;
 import org.apache.commons.lang3.SystemUtils;
 
-public class FriendList {
+class FriendList {
+ 	private String[] fbid ={
+			/*
+			"550694306",
+			"680171822",
+			"655268890",
+			"697702329",
+			"727888809",
+			"100002069773214",
+			"100002077317359",
+			"100002120433942",
+			"100006889091278"
+			"259552347571869" // isamu_ishihara
+			 "100001650247953" //Miyasaka
+			 "100001409430955" // Yasu
+			 */
+			 "100001409430955", // Yasu
+			"100001650247953" //Miyasaka
+	   	};
+ 	
 	public String ReadFB_Account(int no){
-    	String [] fbid ={
-    			/*
-    			"550694306",
-    			"680171822",
-    			"655268890",
-    			"697702329",
-    			"727888809",
-    			"100002069773214",
-    			"100002077317359",
-    			"100002120433942",
-    			"100006889091278"
-    			"259552347571869" // isamu_ishihara
- */
-    			 "100001650247953" //Miyasaka
-    			   	};
     	if(no < fbid.length){
     		return fbid[no];
     	}else{
